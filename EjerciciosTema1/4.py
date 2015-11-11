c26383b14429e1a8b04b772765e594491fcde593
bioinfo <andrea.rubiop88@hotmail.com>
2015-11-11 19:57:27 +0100

    Primer commit

--- EjerciciosTema1/4.py
+++ EjerciciosTema1/4.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 #Realizar un programa que pida el nombre de un fichero al usuario, lo lea y
 # cree otro fichero con el mismo contenido pero con las lineas invertidas
-lineas = []
 nombre = raw_input("Nombre de fichero: ")
 origen = open(nombre)
 destino = open("destino","w")
